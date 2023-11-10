be482d620e5161a4009ac5f8e20e88e3e953d25f
bhavin1216 <136269152+bhavin1216@users.noreply.github.com>
2023-11-10 12:30:22 +0530

    Update face recog.py

--- face recog.py
+++ face recog.py
@@ -1,45 +1,61 @@
 import threading
-
-import cv
+import cv2
 from deepface import DeepFace
 
-cap = cv.VideoCapture(0, cv2.CAP_DSHOW)
 
-cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+
+#To capture the users face
+cap = cv2.VideoCapture(0 , cv2.CAP_DSHOW)
+
+
+#Frame in which the image is captured
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 counter = 0
-face_match = False
-reference_img = cv.imread("reference.jpg")
 
+face_match = False
+
+
+#Reference image used to match the live face
+reference_img1 = cv2.imread("WIN_20231110_11_19_20_Pro.jpg")
+
+
+#Checking whether the face is a match or not
 def check_face(frame):
     global face_match
     try:
-        if DeepFace.verify(frame, reference_img.copy())['verified']:
+        if DeepFace.verify(frame, reference_img1.copy())['verified']:
             face_match = True
         else:
             face_match = False
-    except ValueError:
-        pass
 
+    except ValueError:
+        face_match = False
+
+
+#Scans upto 30 frames and matches the face
 while True:
     ret, frame = cap.read()
 
     if ret:
-        if counter % 30==0:
+        if counter % 30 == 0:
             try:
-                threading.Thread(target=check_face,args=(frame.copy(),)).start()
+                threading.Thread(target=check_face, args=(frame.copy(),)).start()
             except ValueError:
                 pass
         counter += 1
-
+#If face matches then gives name as output
         if face_match:
-            cv.putText(frame, "MATCH!", (20,450),cv.FONT_HERSHEY_SIMPLEX, 2,(0,255,0),3)
+            cv2.putText(frame, "HI BHAVIN!", (20,450), cv2.FONT_HERSHEY_SIMPLEX, 2, (2, 255, 0), 3)
         else:
-            cv.putText(frame, "NO MATCH!", (28,450),cv.FONT_HERSHEY_SIMPLEX, 2, (0,0,255),3)
+            cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (2, 255, 0), 3)
 
-    key = cv.waitkey(1)
+        cv2.imshow("video", frame)
+    key = cv2.waitKey(1)
+
+#'q' is used to stop the live video
     if key == ord("q"):
         break
 
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
